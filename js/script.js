7dba2cf9521c39c8d698930c9baa27e4b01161f9
Beatriz <mllebia@live.com>
2020-09-26 21:25:13 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('Oi');
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCountries = [];
+let favoriteCountries = [];
+
+let countCountries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFavorites = 0;
+
+let numberFormat = null;
+
+window.addEventListener('load', () => {
+  tabCountries = document.querySelector('#tabCountries');
+  tabFavorites = document.querySelector('#tabFavorites');
+
+  countCountries = document.querySelector('#countCountries');
+  countFavorites = document.querySelector('#tabFavorites');
+
+  totalPopulationList = document.querySelector('#totalPopulationList');
+  totalPopulationFavorites = document.querySelector('#totalPopulationFavorites');
+
+  numberFormat = Intl.NumberFormat('en-US');
+  fetchCountries();
+});
+
+function fetchCountries() {
+  console.log('fetching..');
+}
